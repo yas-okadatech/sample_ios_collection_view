1f43b338cf292935b0597f7297a222034e8f14e2
Yas Okada <yas.okadatech@gmail.com>
2015-10-31 21:04:15 +0900

    starting, and flicker api

---
+++ SampleCollectionView/SampleCollectionView/SampleViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+private let reuseIdentifier = "SampleCell"
+private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+
+//key: d18af1254fa67ecfd753aa914de71213
+//secret: d3ce69293a9829c7
+
+class SampleViewController: UICollectionViewController {
+}
