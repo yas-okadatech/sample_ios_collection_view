c8e595ae740408eafc3112f2ecf21f6e3ce794b1
Yas Okada <yas.okadatech@gmail.com>
2015-11-01 11:55:30 +0900

    fetching

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sample_ios_collection_view
+
+## TOD
+
+* [UICollectionView Tutorial Part 1: Getting Started - Ray Wenderlich](http://www.raywenderlich.com/78550/beginning-ios-collection-views-swift-part-1)
--- SampleCollectionView/SampleCollectionView/SampleViewController.swift
+++ SampleCollectionView/SampleCollectionView/SampleViewController.swift
@@ -7,4 +7,63 @@ private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, ri
 //secret: d3ce69293a9829c7
 
 class SampleViewController: UICollectionViewController {
+    private var searches = [FlickrSearchResults]()
+    private let flickr = Flickr()
+    
+    func photoForIndexPath(indexPath: NSIndexPath) -> FlickrPhoto {
+        return searches[indexPath.section].searchResults[indexPath.row]
+    }
+}
+
+extension SampleViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // 1
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        flickr.searchFlickrForTerm(textField.text!) {
+            results, error in
+            
+            //2
+            activityIndicator.removeFromSuperview()
+            if error != nil {
+                print("Error searching : \(error)")
+            }
+            
+            if results != nil {
+                //3
+                print("Found \(results!.searchResults.count) matching \(results!.searchTerm)")
+                self.searches.insert(results!, atIndex: 0)
+                
+                //4
+                self.collectionView?.reloadData()
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension SampleViewController /*: UICollectionViewDataSource*/ {
+    
+    //1
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return searches.count
+    }
+    
+    //2
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searches[section].searchResults.count
+    }
+    
+    //3
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) 
+        cell.backgroundColor = UIColor.blackColor()
+        // Configure the cell
+        return cell
+    }
 }
